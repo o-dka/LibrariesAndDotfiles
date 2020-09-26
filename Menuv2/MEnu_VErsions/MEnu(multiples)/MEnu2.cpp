bb3a6d33265156eadfa84e4bbaec1064fd47633a
Specsaverz <63544489+Specsaverz@users.noreply.github.com>
2020-09-26 16:50:10 +0300

    Re-doing it(again(again)

--- Menuv2/MEnu_VErsions/MEnu(multiples)/MEnu2.cpp
+++ Menuv2/MEnu_VErsions/MEnu(multiples)/MEnu2.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
+void Fibonum(int count,int presentF,int pastF1, int pastF2,int i);
 void ChangetheJ(int curn,int desn,int divn,int subn);
-int main(){
- //for calculation purposes(i.e finding numbers and stuff)
 
+void menu2(int x,int y,int z,int u,int c,int v,int b,int n,int m){
+ //for calculation purposes(i.e finding numbers and stuff)
+do{
+    if
  int x,y,z,u;
     ChangetheJ(x,y,z,u);
  int c,v,b,n,m;
     
-
+}while(i!=4);
 }
 void ChangetheJ(int curn,int desn,int divn,int subn){
  cout<<"please enter your current number"<<endl;
@@ -31,4 +34,21 @@ void ChangetheJ(int curn,int desn,int divn,int subn){
         }
     }
 
+}
+void Fibonum(int count ,int presentF,int pastF1,int pastF1,int i){
+ presentF=1;
+ pastF1=1;
+ pastF2=1;
+ i=3;
+ cout<<"How much of the fibonachi number you want to see?"<<endl;
+ cin>>count;
+ cout<<pastF1<<endl;
+ while(i<=count){
+     presentF=pastF1+pastF2;
+     pastF1 =pastF2;
+     pastF2 =presentF;
+     cout<<presentF<<endl;
+     i++;
+ }
+ cout<<"Here is"<<count<<"of the fibonachi numbers"<<endl;
 }
