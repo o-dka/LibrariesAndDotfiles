eb7031bfc56dbec858add5762af56b478dd8e365
odka <63544489+o-dka@users.noreply.github.com>
2021-02-23 14:35:05 +0300

    formatted

--- Personal_Libs/StupidSort/Stoopidsort.cpp
+++ Personal_Libs/StupidSort/Stoopidsort.cpp
@@ -3,9 +3,9 @@ using namespace std;
 // --------------
 #include "Stoopid.h" // Sorts by using two sort technique 
 void StoopidSort (int *a, int n) {
-	for( int i = 0 ;i < n - 1;i++){
-		for( int j = i+1;j < n;j++){
-			if (a[i] > a[j]){
+	for ( int i = 0 ;i < n - 1;i++) {
+		for ( int j = i+1 ;j < n;j++) {
+			if (a[i] > a[j]) {
 				int c =a[i];
 				a[i] = a[j];
 				a[j] = c;
