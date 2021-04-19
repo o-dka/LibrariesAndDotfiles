5191979f0496704b5af86155284d57e024f19870
odka <63544489+o-dka@users.noreply.github.com>
2021-04-19 12:39:36 +0300

    Renamed the .h file accordingy

--- Personal_Libs/StupidSort/Stoopidsort.cpp
+++ Personal_Libs/StupidSort/Stoopidsort.cpp
@@ -1,5 +1,5 @@
 #include "Stoopid.h" // Sorts by using two sort technique 
-void StoopidSort (int *a, int n) {
+void StoopidSort (int *a, int n) { // Input is an array and an amount of numbers in it 
 	for ( int i = 0 ;i < n - 1;i++) {
 		for ( int j = i+1 ;j < n;j++) {
 			if (a[i] > a[j]) {
