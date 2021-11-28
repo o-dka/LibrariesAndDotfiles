3bcbb7cee78e465c4350b7392e3023d37c0ddb98
odka <63544489+o-dka@users.noreply.github.com>
2021-11-28 17:41:39 +0300

    Update main.cpp
    23 line is only needed if COUNTER < 10

--- Mini_Projects/half_calc/main.cpp
+++ Mini_Projects/half_calc/main.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <string>
-#define COUNTER 7
+#define COUNTER 10
 using namespace std;
 double formula(double y, int pew, double input) { return pow(y, pew) - input; }
 
@@ -11,7 +11,7 @@ int main()
 	int in_fact;
 	cin >> left_br >> right_br >> in_fact >> in_number;
 	printf("=========================================================================\n");
-	printf(" L | R | M || L_v | M_v | R_v \n");
+	printf("L         |R         |M         ||L_v        |M_v       |R_v           |\n");
 	printf("=========================================================================\n");
 	int count = COUNTER;
 	while (count--) {
@@ -20,14 +20,12 @@ int main()
 		right_br_res = formula(right_br, in_fact, in_number);
 		mid_res = formula(mid, in_fact, in_number);
 		printf(" %f | %f | %f || %f | %f | %f |\n", left_br, right_br, mid, left_br_res, right_br_res, mid_res);
-		if (count > 1) {
+//      if (count > 1) {
 			if (mid_res < 0)
 				left_br = mid;
 			else if (mid_res > 0)
 				right_br = mid;
-		}
-		else
-			continue;
+// 		}
 	}
 	double print_val = right_br > left_br ? right_br : left_br;
 	printf("%0.3f\n", print_val);
