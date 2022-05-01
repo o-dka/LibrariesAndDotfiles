c3d3950f6080178316da41d031a1d22b0b546f0e
o-dka <63544489+o-dka@users.noreply.github.com>
2022-05-01 19:50:56 +0300

    syncing

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+# More info : https://github.com/jaagr/polybar/wiki
+
+# Install the following applications for polybar and icons in polybar if you are on ArcoLinuxD
+# awesome-terminal-fonts
+# Tip : There are other interesting fonts that provide icons like nerd-fonts-complete
+# --log=error
+# Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
@@ -14,10 +21,20 @@ case $desktop in
     bspwm|/usr/share/xsessions/bspwm)
     if type "xrandr" > /dev/null; then
       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload mainbar -c ~/.config/polybar/config &
+        MONITOR=$m polybar --reload mainbar -c ~/.config/polybar/config.ini &
       done
     else
-    polybar --reload mainbar -c ~/.config/polybar/config &
+    polybar --reload mainbar -c ~/.config/polybar/config.ini &
+    fi
+    ;;
+
+    spectrwm|/usr/share/xsessions/spectrwm)
+    if type "xrandr" > /dev/null; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload mainbar-spectrwm -c ~/.config/polybar/config.ini &
+      done
+    else
+    polybar --reload mainbar-spectrwm -c ~/.config/polybar/config.ini &
     fi
     ;;
 
--- .config/spectrwm/scripts/xwalltool.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-xwallpaper --center ~/Pictures/wall.png
-echo "wallpaper set"
